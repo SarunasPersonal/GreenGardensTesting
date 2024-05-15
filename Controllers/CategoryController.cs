5fbd1a7a4463bafe0dfe93dd2d420abf926f68f6
Sarunas Slekys <52355769+SarunasPersonal@users.noreply.github.com>
2024-05-15 12:14:02 +0100

    UnitOfWork Implamented

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,32 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using GreenGardens.DataAccess;
 using GreenGardens.Models;
-using GardensGardens.AccessData;
 using GreenGardens.DataAccess.Repository.IRepository;
 
 namespace Gardens.Controllers
 {
-    //Category Controller allows to view the Category page
-    //depency injection is used to inject the ICategoryRepository interface
-    /// dependency injection is a technique in which an object receives other objects that it depends on
     public class CategoryController : Controller
     {
-        private readonly ICategoryRepository _categoryRepo;
-        public CategoryController(ICategoryRepository db)
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
         {
-            _categoryRepo = db;
+            _unitOfWork = unitOfWork;
         }
         public IActionResult Index()
         {
-            //retrieve the list of categories from the database
-            //.ToList() method is used to convert the data into a list
-            List<Category> objCatergoryList = _categoryRepo.GetAll().ToList();
-            //return the list of categories to the view
-            return View(objCatergoryList);
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+            return View(objCategoryList);
         }
         public IActionResult Create()
         {
@@ -45,15 +33,14 @@ namespace Gardens.Controllers
 
             if (ModelState.IsValid)
             {
-                _categoryRepo.Add(obj);
-                _categoryRepo.Save();
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
                 TempData["success"] = "Category Added Successfully";
                 return RedirectToAction("Index");
             }
-            return View();
+            return NotFound();
 
         }
-        //divoded
 
 
 
@@ -68,9 +55,8 @@ namespace Gardens.Controllers
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _categoryRepo.Get(u => u.Id == id);
-            //Category? CategoryFromDB1 = _db.Categories.FirstOrDefault(u => u.Id == id);
-            //Category? CategoryFromDB2 = _db.Categories.Find(id);
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
+
 
             if (categoryFromDb == null)
             {
@@ -82,32 +68,24 @@ namespace Gardens.Controllers
 
 
         [HttpPost]
-
         public IActionResult Edit(Category obj)
         {
-
-
             if (ModelState.IsValid)
             {
-                _categoryRepo.Update(obj);
-                _categoryRepo.Save();
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
                 TempData["success"] = "Category Edited Successfully";
                 return RedirectToAction("Index");
             }
             return View();
-
         }
-
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
             {
                 return NotFound();
             }
-            //find the category from the database based on the id can be used to find one category
-            Category? categoryFromDb = _categoryRepo.Get(u => u.Id == id);
-            //Category? CategoryFromDB1 = _db.Categories.FirstOrDefault(u => u.Id == id);
-            //Category? CategoryFromDB2 = _db.Categories.Find(id);
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
 
             if (categoryFromDb == null)
             {
@@ -116,25 +94,18 @@ namespace Gardens.Controllers
             return View(categoryFromDb);
         }
 
-
-
         [HttpPost, ActionName("Delete")]
         public IActionResult DeletePOST(int? id)
         {
-            Category? obj = _categoryRepo.Get(u => u.Id == id);
+            Category? obj = _unitOfWork.Category.Get(u => u.Id == id);
             if (obj == null)
             {
                    return NotFound();
             }
-
-           
-                _categoryRepo.Remove(obj);
-                _categoryRepo.Save();
-                TempData["success"] = "Category Deleted Successfully";
-                return RedirectToAction("Index");
-            
-            
-
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
         }
     
     }
